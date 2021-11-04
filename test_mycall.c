3feff8968ebeebc5499e778234c837c787f45f35
Quidifer <christophermouri@gmail.com>
2021-11-04 00:14:20 -0700

    finished lab2 tests

--- test_mycall.c
+++ test_mycall.c
@@ -65,8 +65,92 @@ main(int argc, char *argv[]) {
   else {
     sleep(5);
     printf(1, "\n\nIm the child. I ran with priority: %d\n\n", get_priority());
+    exit(0);
+  }
+
+  sleep(5);
+
+    //------------ Testing scheduling performance  ---------------------------
+  printf(1, "\n ----------------------- Testing scheduling performance ----------------------\n");
+  pid1 = fork();
+  if (pid1) {
+    set_priority(15);
+    while (!waitpid(pid1, 0, 1)) {
+      printf(1, "I'm the parent. I had to wait for %d time before running.\n", time());
+    }
+  } else {
+    sleep(5);
+    printf(1, "\n\n I'm the child. I had to wait for %d time before running.\n", time());
+  }
+
+  sleep(5);
+
+  pid1 = fork();
+  if (pid1) {
+    set_priority(11);
+    while (!waitpid(pid1, 0, 1)) {
+      printf(1, "I'm the parent. My current turnaroud time is %d.\n", turnaround());
+    }
+  } else {
+    sleep(5);
+    printf(1, "\n\n I'm the child. My current turnaround time is %d.\n", turnaround());
   }
 
   exit(0);
   return 0;
 }
+
+// #include "types.h"
+// #include "user.h"
+
+// int main(int argc, char *argv[])
+// {
+	
+// 	int PScheduler(void);
+
+//   printf(1, "\n This program tests the correctness of your lab#2\n");
+  
+// 	PScheduler();
+// 	return 0;
+// }
+  
+    
+// int PScheduler(void){
+		 
+//     // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 31
+//     // 0 is the highest priority and 31 is the lowest priority.  
+
+//   int pid;
+//   int i,j,k;
+  
+//   printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
+//   printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
+//   printf(1, "\n  Step 2: 0 is the highest priority. All processes have a default priority of 10\n");
+//   printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
+//   set_priority(0);
+//   for (i = 0; i <  3; i++) {
+// 	  pid = fork();
+//     if (pid > 0 ) {
+//       continue;}
+//     else if ( pid == 0) {
+
+//       set_priority(30-10*i);	
+//       for (j=0;j<50000;j++) {
+//         for(k=0;k<1000;k++) {
+//           asm("nop"); }}
+//       printf(1, "\n child# %d with priority %d has finished! get_priority():%d \n",getpid(),30-10*i, get_priority());		
+//       exit(0);
+//     }
+//     else {
+//       printf(2," \n Error \n"); 
+//     }
+// 	}
+
+// 	if(pid > 0) {
+// 		for (i = 0; i <  3; i++) {
+// 			wait(0);
+// 		}
+//     printf(1,"\n if processes with highest priority finished first then its correct \n");
+//   }
+// 	exit(0);		
+// 	return 0;}
